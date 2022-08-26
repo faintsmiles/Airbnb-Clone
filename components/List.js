02518f90f6d9a9533b899b166b2cfcf841169abf
faint <ifaintsmile@gmail.com>
2022-08-26 01:07:33 -0700

    fixed scaling and sizing on listing  cards

--- components/List.js
+++ components/List.js
@@ -25,12 +25,13 @@ export default function List() {
 
     return (
     <>
-      <div className='px-2 my-10 gap-4 sm:grid md:grid-cols-2 xl:grid-cols-5 3xl:flex flex-wrap justify-center relative'>
+      <div className='px-4 my-24 gap-4 gap-y-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 md:px-15 lg:grid-cols-4 lg:px-24   xl:grid-cols-5 3xl:flex flex-wrap justify-center relative'>
         { data.data.map((result) => {
             return <Thumbnail 
             listingData={result.fields} 
             location={result.geometry} 
             key={result.recordid} 
+            className='relative items-stretch h-full flex-col group'
             />
           }) 
         }
