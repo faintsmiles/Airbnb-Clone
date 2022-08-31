96928f63aa3c7c2af1ac3c607c0943023b3fd251
faint <ifaintsmile@gmail.com>
2022-08-30 19:34:18 -0700

    cleaned up api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -6,30 +6,18 @@ export default async function handler(req, res) {
    
    let cityName = 'Amsterdam'
    let countryName = 'Netherlands'
+   
    // Find user's ip
-   // const forwarded = req.headers["x-forwarded-for"]
-   // const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
-   // console.log(ip)
-
-   // var ip_addr = req.headers['X-FORWARDED-FOR'] || req.connection.remoteAddress;
-   // console.log(ip_addr)
-   require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-      console.log('addr: ' + add );
-    })
+   require('dns').lookup(require('os').hostname(), function (err, add, fam) { console.log('addr: ' + add ) })
 
    if(req.body){
       const body = JSON.parse(req.body)
       cityName = body.city;
-      // countryName = body.country;
+      countryName = body.country;
    }
-   //  if(req.body) {
 
-   //  }
-
-   //const apiURL = `https://public.opendatasoft.com/api/records/1.0/search/?dataset=airbnb-listings&q=&facet=city&facet=country&facet=property_type&facet=room_type&facet=bed_type&facet=amenities&facet=availability_365&facet=cancellation_policy&facet=features&refine.country=${countryName}&refine.city=${cityName}`
    const apiURL = `https://public.opendatasoft.com/api/records/1.0/search/?dataset=airbnb-listings&q=&facet=host_response_time&facet=host_response_rate&facet=host_verifications&facet=city&facet=country&facet=property_type&facet=room_type&facet=bed_type&facet=amenities&facet=availability_365&facet=cancellation_policy&facet=features&refine.city=${cityName}`
-   console.log(apiURL)
-
+   console.log("api url:" + apiURL)
 
    return fetch(apiURL)
     .then((response) => response.json())
