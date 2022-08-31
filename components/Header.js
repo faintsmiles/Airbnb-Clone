caccc5602188ebe5839c82c6e3ea923016afeb79
faint <ifaintsmile@gmail.com>
2022-08-30 21:09:24 -0700

    added QOL functionality to the search bar

--- components/Header.js
+++ components/Header.js
@@ -13,7 +13,9 @@ export default function Header({ setSearchLocation }) {
 
   // Submit handler for search input 
   const handleSearchSubmit = async (e) => {
-    e.preventDefault();
+    if(e) {
+      e.preventDefault();
+    }
     console.log("handle search submission")
     setSearchLocation(searchInputValue.current.value);
   } 
@@ -27,7 +29,7 @@ export default function Header({ setSearchLocation }) {
         <form className="search-container flex items-center w-full  md:max-w-xs" onSubmit={(e) => handleSearchSubmit(e)}>
           <label htmlFor="simple-search" className="sr-only">Search</label>
           <div className="relative w-full">
-            <Autocomplete options={searchOptions} >
+            <Autocomplete options={searchOptions} onPlaceChanged={(e)=>handleSearchSubmit(e)} >
               <input type="text" id="simple-search" 
                 className="bg-gray-50 border border-gray-300 text-sm rounded-2xl focus:ring-blue-500 focus:border-blue-500 block w-full pr-10 p-2.5 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500 shadow-lg" 
                 placeholder="Start your search" 
@@ -35,11 +37,11 @@ export default function Header({ setSearchLocation }) {
                 required 
               />
             </Autocomplete>
-            <div className="flex absolute inset-y-0 right-0 items-center pr-2 pointer-events-none">
+            <button className="flex absolute inset-y-0 right-0 items-center pr-2 ">
               <div className=" bg-brand-color rounded-3xl p-1.5 ">
                 <svg aria-hidden="true" className="w-5 h-5" fill="white" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
               </div>
-            </div>
+            </button>
           </div>
         </form>
         <div className="hidden md:flex justify-between space-x-4" >
