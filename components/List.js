2aecbae14f7b375e01c8ca61e0248aeabf96f4db
faint <ifaintsmile@gmail.com>
2022-11-29 21:00:06 -0800

    changed how we were storing results' data in index and corresponding child components. added initial default location. changed filter modal to return results based on current location

--- components/List.js
+++ components/List.js
@@ -5,14 +5,14 @@ import Thumbnail from './Thumbnail'
 export default function List({results, isLoading}) {
     // if (isLoading) return <p>Loading...</p>
     console.log(results)
-    if (!results || !results.data) return <p className='className="absolute p-4 w-full h-max border z-50 bg-white text-black font-bold text-center mx-auto "'>Results could not be fetched. Please try refreshing</p>
-    if(results.data.length === 0) return <div className="p-4 w-full h-max border z-50 bg-white text-black font-bold text-center mx-auto">No results found</div>
+    if (!results ) return <p className='absolute p-4 w-full h-max border z-50 bg-white text-black font-bold text-center mx-auto'>Results could not be fetched. Please try refreshing</p>
+    if(results.length === 0) return <div className='p-4 w-full h-max border z-50 bg-white text-black font-bold text-center mx-auto'>No results found</div>
     
     return (
       <>
         <div className='px-4 my-24 gap-4 gap-y-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:px-24 2xl:grid-cols-5 2xl:px-44 3xl:flex flex-wrap justify-center relative'>
           {   
-              results.data.map((element) => {
+              results.map((element) => {
               return <Thumbnail 
                   results={element.fields} 
                   location={element.geometry} 
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
    return fetch(apiURL + countryName + cityName )
     .then((response) => response.json())
     .then((data) => {
-       res.status(200).json({ data: data.records });
+       res.status(200).json({ data: data.records, defaultLocation: cityName.split('=')[1]  || cityName });
     })
     .catch((err) => {
        res.status(404).json({ Message: err });
