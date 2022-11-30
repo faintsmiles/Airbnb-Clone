ff1506e9723747c85d069fbc2d7793c35cf9fdb4
faint <ifaintsmile@gmail.com>
2022-11-29 21:18:06 -0800

    added values to property types to match query choices. can now filter by property types.

--- components/common/FilterModalMenuButtonIcon.js
+++ components/common/FilterModalMenuButtonIcon.js
@@ -3,13 +3,14 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faBuilding, faHouseUser, faHotel } from '@fortawesome/free-solid-svg-icons';
 
-export default function FilterModalMenuButtonIcon({type, property, setProperty, icon}) {
+export default function FilterModalMenuButtonIcon({type, value, property, setProperty, icon}) {
 
 
   return (
       <div 
-        className={`flex-1 flex flex-col p-4 gap-8 border rounded-lg hover:border-black hover:cursor-pointer ${type === property ? 'bg-black text-white' : '' } `} 
-        onClick={(e) => setProperty(type)}
+        className={`flex-1 flex flex-col p-4 gap-8 border rounded-lg hover:border-black hover:cursor-pointer ${value === property ? 'bg-black text-white' : '' } `} 
+        
+        onClick={(e) => setProperty(value)}
       >
         <div>
           <FontAwesomeIcon className='w-8 h-8' icon={icon} />
