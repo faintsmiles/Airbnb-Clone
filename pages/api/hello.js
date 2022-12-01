9f09682e78da6f58135b04a04993ce0559a1c3b2
faint <ifaintsmile@gmail.com>
2022-11-30 23:05:52 -0800

    changed url, removed country from search

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 // const apiURL = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=airbnb-listings&q=&rows=20&facet=host_response_time&facet=host_response_rate&facet=host_verifications&facet=city&facet=country&facet=property_type&facet=room_type&facet=bed_type&facet=amenities&facet=availability_365&facet=cancellation_policy&facet=features'
-const apiURL = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=airbnb-listings&q=&rows=20&facet=host_response_time&facet=host_response_rate&facet=host_verifications&facet=city&facet=country&facet=property_type&facet=room_type&facet=bed_type&facet=amenities&facet=availability_365&facet=cancellation_policy&facet=features'
+const apiURL = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=airbnb-listings&q=&rows=30'
 //const apiURL ='';
 
 export default async function handler(req, res) {
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 
    console.log("api url:" + apiURL + countryName + cityName )
 
-   return fetch(apiURL + countryName + cityName )
+   return fetch(apiURL + cityName )
     .then((response) => response.json())
     .then((data) => {
        res.status(200).json({ data: data.records, defaultLocation: cityName.split('=')[1]  || cityName });
