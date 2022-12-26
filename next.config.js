69b304d5cba380da0c5ac37486c5bd8baabd1dd4
faint <ifaintsmile@gmail.com>
2022-12-25 16:15:47 -0800

    no longer optimizing images

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   images: {
     domains: ["a0.muscache.com", "a1.muscache.com", "a2.muscache.com", "https://muscache.com" ],
     minimumCacheTTL: 1200,
+    unoptimized: true,
   },
   swcMinify: true,
 }
