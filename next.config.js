ce3bd64d74f65f078fdb984fa7629fca77b69773
faint <ifaintsmile@gmail.com>
2022-12-25 16:20:52 -0800

    images are back to being optimized.. looking into bug

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   images: {
     domains: ["a0.muscache.com", "a1.muscache.com", "a2.muscache.com", "https://muscache.com" ],
     minimumCacheTTL: 1200,
-    unoptimized: true,
   },
   swcMinify: true,
 }
